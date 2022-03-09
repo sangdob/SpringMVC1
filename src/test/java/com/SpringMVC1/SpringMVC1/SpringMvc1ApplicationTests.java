f59a620d83138b2bbc3f6df86e8a438bafd086e3
sangdob <51118110+sangdob@users.noreply.github.com>
2022-03-10 01:37:50 +0900

    init Commit

---
+++ src/test/java/com/SpringMVC1/SpringMVC1/SpringMvc1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringMVC1.SpringMVC1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvc1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
