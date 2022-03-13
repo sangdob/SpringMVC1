3eb57f7e61eb1145fd371fa9bc6bcdf803d1f0cf
snagdob <skql1358@gmail.com>
2022-03-13 20:04:21 +0900

    1. hello서블릿
    서블릿 초기 테스트 용도

--- src/main/java/com/SpringMVC1/SpringMVC1/Servlet/HelloServlet.java
+++ src/main/java/com/SpringMVC1/SpringMVC1/Servlet/HelloServlet.java
@@ -4,11 +4,13 @@ import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@WebServlet(name="helloServlet", urlPatterns = "/hello")
 public class HelloServlet extends HttpServlet {
 
     @Override
@@ -16,11 +18,19 @@ public class HelloServlet extends HttpServlet {
         System.out.println("init method");
     }
 
+    // servlet test용도 response, request 주소값 out
     @Override
-    public void service(ServletRequest req, ServletResponse res)
-            throws ServletException, IOException {
-        System.out.println("service method");
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        super.service(req, resp);
+        System.out.println("HelloServlet.service");
+        System.out.println("req = " + req + ", resp = " + resp);
     }
+
+//    public void service(ServletRequest req, ServletResponse res)
+//            throws ServletException, IOException {
+//        System.out.println("service method");
+//        System.out.println("req = " + req + ",\n res = " + res);
+//    }
 
     @Override
     public void destroy(){
